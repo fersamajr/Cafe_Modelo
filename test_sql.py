c64cba23d83ad7ee6da911ef958acba1cd16c7fa
fersamajr <63007492+fersamajr@users.noreply.github.com>
2025-10-27 02:05:45 +0000

    a

---
+++ test_sql.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+try:
+    conn = mysql.connector.connect(
+        host="2.tcp.us-cal-1.ngrok.io",
+        port=13185,
+        user="root",
+        password="Fp$c0105",
+        database="Cafe"   # o el nombre real
+    )
+    print("¡Conexión exitosa con ngrok!")
+    conn.close()
+except Exception as e:
+    print("Error de conexión:", e)
